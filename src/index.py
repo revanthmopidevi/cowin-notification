dc4cbbf6ad3b347146d9b8474788313436efb133
Revanth <revanthmopidevi@gmail.com>
2021-05-29 16:33:30 +0530

    status

--- src/index.py
+++ src/index.py
@@ -12,9 +12,18 @@ PHONE = config('PHONE')
 STATE = config('STATE')
 DISTRICT = config('DISTRICT')
 
-to = ['+917977480148', '+919182787502', '+917013385235', '+919182920872', '+917207761909', '+919100562917']
+to = [
+    '+917977480148',
+    '+919182787502', 
+    '+917013385235', 
+    '+919182920872', 
+    '+917207761909',
+    '+919100562917',
+    '+919849334737'
+]
 
 while (True):
     time.sleep(1)
     if (slots(districtID(DISTRICT, stateID(STATE)))):
         teleMessage(API_ID, API_HASH, PHONE, to, f'Slot Available on https://www.cowin.gov.in/home for 18+ dose 1')
+        teleMessage(API_ID, API_HASH, PHONE, '+917977480148', 'App is Running.')
